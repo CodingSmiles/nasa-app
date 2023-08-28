064aac514908259171e5ecc18932bfeb679a9af3
CodingSmiles <aadirajanil726@gmail.com>
2023-08-28 17:25:58 +0530

    Malishka

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
-import "flutter/"
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(main());
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Hello World!"),
+      ),
+    );
+  }
+}
