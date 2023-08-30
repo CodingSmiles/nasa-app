3dd2489a8bae0b75ac67387933961292b2e20341
CodingSmiles <aadirajanil726@gmail.com>
2023-08-30 10:44:32 +0530

    malishka 2

--- lib/glass_button.dart
+++ lib/glass_button.dart
@@ -4,17 +4,30 @@ import 'package:flutter/material.dart';
 
 // create a stateless widget with a build method that returns a card with glassmorphism effect and rounded corners with one title
 class GlassButton extends StatelessWidget {
-  Color selectColor() {
+  List<Color> selectColor() {
     var rng = Random();
     var colors = [
-      const Color.fromRGBO(65, 151, 211, 1),
-      const Color.fromRGBO(214, 32, 73, 0.6),
-      // const Color.fromRGBO(246, 162, 179, 1),
-      const Color.fromRGBO(68, 112, 48, 1.0),
-      // const Color.fromRGBO(248, 181, 193, 1),
-      // const Color.fromRGBO(227, 245, 219, 1),
-      const Color.fromRGBO(255, 181, 50, 0.7),
-      const Color.fromRGBO(105, 3, 34, 0.5),
+      [
+        const Color.fromRGBO(65, 151, 211, 1),
+        const Color.fromRGBO(255, 255, 255, 1)
+      ],
+      [
+        const Color.fromRGBO(214, 32, 73, 0.6),
+        const Color.fromRGBO(255, 255, 255, 1),
+      ],
+      [
+        const Color.fromRGBO(68, 112, 48, 1.0),
+        const Color.fromRGBO(255, 255, 255, 1),
+      ],
+      [
+        const Color.fromRGBO(255, 181, 50, 0.7),
+        const Color.fromRGBO(0, 0, 0, 1)
+      ],
+
+      [
+        const Color.fromRGBO(105, 3, 34, 0.5),
+        const Color.fromRGBO(255, 255, 255, 1),
+      ],
     ];
     return colors[rng.nextInt(colors.length)];
   }
@@ -26,14 +39,11 @@ class GlassButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // get screen size
-    final Size size = MediaQuery.of(context).size;
-    // get screen width
-    final double width = size.width;
-    // get screen height
-    final double height = size.height;
-    // get screen width divided by 100
+    final Size size = MediaQuery.of(context).size; // get screen size
+    final double width = size.width; // get screen width
+    final double height = size.height; // get screen height
     final double hpw2 = (height + width) / 200;
+    final List<Color> colorS = selectColor();
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -52,7 +62,7 @@ class GlassButton extends StatelessWidget {
         ),
         child: Card(
           elevation: 10,
-          color: selectColor(),
+          color: colorS[0],
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(28.0),
           ),
@@ -68,7 +78,7 @@ class GlassButton extends StatelessWidget {
                     text,
                     style: TextStyle(
                       fontFamily: "Inter",
-                      color: Colors.white,
+                      color: colorS[1],
                       fontSize: hpw2 * 3.8,
                       fontWeight: FontWeight.w900,
                     ),
